97dddd156e8d2415331092ae40dd32b6d8539d8e
Joshua Panchana <jpanchana0902@icloud.com>
2024-05-29 23:52:46 -0400

    Add my info structure

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import Projects  from './Projects';
 
 const About = () => {
     return (
         <div className="my-5">
             <h2>About Me</h2>
-            <p>Information about me goes here.</p>
+            <p>I am a Computer Science student at the University of Akron. In addition to pursuing my Bachelor's degree, I am also pursuing
+             a minor in Applied Mathematics and Spanish. During my years at Akron, I have had two internship rotations at Goodyear Rubber and Tire Company.
+             In the recent rotations, I created unit test in python to test functions prior to it being used by AWS services.
+             Outside of education and my professional work, I tend to practice what I learn into my own project to apply what I have learned and to review. This site
+             is a result of that as I am interested in web development and programming.
+
+             I have experience in JavaScript, React, C++, AWS Lamdas, SQL, and Python. I have worked with Github, Github actions, Docker, Git, Jira, and Azure DevOps. 
+             I am also educated in modern DevOps operations and am familiar with Agile methodologies. Refer to my Projects{Projects} page for more.
+
+             Seeking: I am currently seeking a full-time position in software development or a related field. I am open to relocation and remote work opportunities.
+
+             <h2>Background:</h2>
+                I was born in Brooklyn, New York. Both of my parents are from Ecuador. I am bi lingual
+
+            </p>
         </div>
     );
 };
