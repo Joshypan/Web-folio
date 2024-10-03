a2de1778a8730f77807e608f5ef1c9b768a82020
Joshua Panchana Cedeño <jpc@Mac.attlocal.net>
2024-10-03 00:17:01 -0400

    Update About page

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -6,13 +6,13 @@ const About = () => {
             <h2>About Me</h2>
             <p> 
                 Hello! I am Joshua Panchana Cedeño, a Computer Science student at the University of Akron. In addition to pursuing my Bachelor's degree, I am also pursuing two minor
-                in Applied Mathematics and one in Spanish. My current cummulative GPA is a 3.4. <br/><br/>
+                in Applied Mathematics and one in Spanish. My current cumulative GPA is a 3.4. <br/><br/>
 
-                At the University of Akron, I am heavily invovled on campus. During my second year in college, I was the President 
-                of UA Latinos and a general member of Society of Hispanic Professional Engineers (SHPE). UA Latinos is an orangization that aimed to connect and support the Latino and Hispanic population on campus. As President, I strived to grow the number of 
-                members in the orangization and make the club standout, to let others aware of our presence! After two successful years, I am proud to say that the membership
+                At the University of Akron, I am heavily involved on campus. During my second year in college, I was the President 
+                of UA Latinos and a general member of Society of Hispanic Professional Engineers (SHPE). UA Latinos is an organization that aimed to connect and support the Latino and Hispanic population on campus. As President, I strived to grow the number of 
+                members in the organization and make the club standout, to let others aware of our presence! After two successful years, I am proud to say that the membership
                 count has tripled from 10 to 30. In addition it has been awarded a Life Award for Diversity in 2023. Afterwards, in my fourth year (Fall 2023 - Spring 2024), I was appointed as Vice President
-                of the SHPE. SHPE is an orangization that aids its' members in their professional development. We collaborate with other engineering societies on campus and collaborate
+                of the SHPE. SHPE is an organization that aids its' members in their professional development. We collaborate with other engineering societies on campus and collaborate
                 with local companies such as Goodyear, Marathon, Nestle, and Lubrizol (to name a few). During the spring semester, I became a member of the Akronauts, the rocket design
                 team on campus. I would help with the development on their graphic user interface for the test stands' components.
 
